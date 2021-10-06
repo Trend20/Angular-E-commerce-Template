9390ff65894571db819eff0fb51a36d94ec91981
DevEnock <enockomondi305@gmail.com>
2021-10-06 11:37:43 +0300

    cleaning the root project directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button'; 
 import {MatIconModule} from '@angular/material/icon'; 
 import {MatInputModule} from '@angular/material/input';
-import { PagesComponent } from './pages/pages.component'; 
 import {MatTabsModule} from '@angular/material/tabs';
-import { NewProductsComponent } from './pages/new-products/new-products.component';
-import { SidebarComponent } from './pages/sidebar/sidebar.component'; 
 import {MatExpansionModule} from '@angular/material/expansion'; 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component:  TopBarComponent},
     ]),
     BrowserAnimationsModule,
     MatButtonModule,
@@ -29,10 +24,6 @@ import {MatExpansionModule} from '@angular/material/expansion';
   ],
   declarations: [
     AppComponent,
-    PagesComponent,
-    NewProductsComponent,
-    SidebarComponent,
-    TopBarComponent
   ],
   bootstrap: [
     AppComponent
